07c413d2fa6ca1b52ed3b7c31a93cd28f18f07f1
Polka Oliver <polka.oliver@gmail.com>
2019-03-26 14:38:48 +0100

    simple update items implementation

--- index.php
+++ index.php
@@ -47,8 +47,8 @@ $merchant_token = $json_data->token;
 // Exhaustive list of all possibilities available here: <https://docs.avarda.com/?post_type=checkout30&p=1552#initialize-payment>
 // Merchant has to send merchant access token as an authorization in the POST request header:
 //      Authorization: Bearer <merchant_access_token_here>
-// Successfull initialization returns unique JWT customer access token and purchase ID
-// Customer access token is used to display checkout form on the frontend for the current session
+// Successfull initialization returns unique JWT session access token and purchase ID
+// Session access token is used to display checkout form on the frontend for the current session
 $init_payment_url = "$api_url/api/merchant/initializePayment";
 $payment_data = array(
     "language" => "English", "items" => array(array(
@@ -72,16 +72,50 @@ $init_result = file_get_contents($init_payment_url, false, $cont);
 if ($init_result === false) { /* Handle error */ };
 
 $init_data = json_decode($init_result);
-$access_token = $init_data->jwt;
+$session_access_token = $init_data->jwt;
 $purchase_id = $init_data->purchaseId;
 
 
-// Encode customer access token so it can be displayed in the URL
-$encoded_access_token = urlencode($access_token);
+// Encode session access token so it can be displayed in the URL
+$encoded_access_token = urlencode($session_access_token);
 if (empty($_GET['accessToken'])) {
     header("Location: http://localhost:8000/?accessToken=$encoded_access_token");
     die();
 };
+
+// Update items in the current session 
+// Merchant has to send merchant access token as an authorization in the POST request header:
+//      Authorization: Bearer <merchant_access_token_here>
+// Merchant has to provide a purchaseId that was obtained after POST call to /api/merchant/initializePayment
+// in order to send a new list of items
+if (!empty($_GET['updateItems'])) {
+    $update_amount = rand(10, 500);
+    $tax_amount = $update_amount * 0.2;
+
+    $update_items_url = "$api_url/api/merchant/updateItems";
+    $update_data = array(
+        "purchaseId" => $purchase_id, "items" => array(array(
+            "description" => "Some item",
+            "notes" => "",
+            "amount" => (int)$update_amount,
+            "taxCode" => "20",
+            "taxAmount" => (int)$tax_amount
+        )),
+    );
+
+    $opts = array(
+        'http' => array(
+            'header'  => "Accept: application/json\r\nContent-type: application/json-patch+json\r\nAuthorization: Bearer $merchant_token\r\n",
+            'method'  => 'POST',
+            'content' => json_encode($update_data)
+        )
+    );
+    $cont  = stream_context_create($opts);
+    $update_result = file_get_contents($update_items_url, false, $cont);
+    if ($update_result === false) { /* Handle error */ };
+
+    $update_response = json_decode($update_result);
+}
 ?>
 
 <!doctype html>
@@ -96,18 +130,18 @@ if (empty($_GET['accessToken'])) {
 
 <body>
     <h1>AVARDA - PHP Integration Demo</h1>
-
+    <button><a href="/?accessToken=<?php echo (string)$encoded_access_token ?>&updateItems=1">Update Items</a></button>
     <!-- Checkout form will be displayed in a <div> with a custom unique ID, this ID is passed on in the checkout app initialization -->
     <div id="checkout-form"></div>
     <!-- Include one script with all neccessary JS code for the checkout app in your source code -->
     <script src="<?php echo (string)$checkout3_js_bundle ?>"></script>
     <!-- During the initialization of the checkout app, additional flags can be passed to change appearance or behaviour of the app -->
-    <!-- Customer access token is passed and required -->
+    <!-- Session access token is passed and required -->
     <!-- Redirect url is neccessary for payment methods that will redirect user to their domain while processing payment (e.g. card payment) -->
     <!-- Additional information available here: <https://docs.avarda.com/?post_type=checkout30&p=1552#showing-the-form> -->
     <script>
         window.avardaCheckoutInit({
-            "accessToken": "<?php echo (string)$access_token ?>",
+            "accessToken": "<?php echo (string)$session_access_token ?>",
             "rootElementId": "checkout-form",
             "redirectUrl": "<?php echo (string)$public_url ?>",
             "styles": {},
@@ -116,7 +150,7 @@ if (empty($_GET['accessToken'])) {
     </script>
     <hr>
     <!-- Refresh the page restarting the process of authentication and payment initialization, new token will be created for the session -->
-    <button><a href="/">Reset Access Token</a></button>
+    <button><a href="/">Reset Session Access Token</a></button>
 </body>
 
 </html> 
