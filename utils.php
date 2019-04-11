2249cd3bc36b519b591911a632e4a2b75461fcb4
Polka Oliver <polka.oliver@gmail.com>
2019-04-11 14:18:47 +0200

    Annotate the function with some more info

--- utils.php
+++ utils.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Send a POST Request
+ *
+ * Uses `file_get_contents()` to send a POST request, takes request url, header and payload as arguments
+ *
+ * @link https://www.php.net/manual/en/function.file-get-contents.php
+ *
+ * @param string $request_url
+ * @param string $request_header 
+ * @param array $request_payload
+ * @return string
+ */
 function send_post_request($request_url, $request_header, $request_payload) {
     $options = array(
         'http' => array(
@@ -10,5 +22,4 @@ function send_post_request($request_url, $request_header, $request_payload) {
     $context  = stream_context_create($options);
     $result = file_get_contents($request_url, false, $context);
     return $result;
-};
-?>
+}
